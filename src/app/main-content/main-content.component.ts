294f5ae3fb07f7589ebf4b167fd0e33d513580ef
Hummner <bence.cservenyak@gmail.com>
2025-05-27 20:21:59 +0200

    our menu and how to order section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HowToOrderComponent } from "./how-to-order/how-to-order.component";
+import { OurMenuComponent } from "./our-menu/our-menu.component";
 
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent],
+  imports: [LandingPageComponent, HowToOrderComponent, OurMenuComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
